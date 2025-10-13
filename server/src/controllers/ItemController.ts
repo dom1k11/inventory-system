222c02565a730aace4a047aa68a34094ec4afa2a
Dominik Sedusov <dom1k11@outlook.com>
2025-10-13 21:15:38 +0300

    endpoint changed to add item entity+custom fields to it

--- server/src/controllers/ItemController.ts
+++ server/src/controllers/ItemController.ts
@@ -1,8 +1,11 @@
-import { addItem } from '../services/addEmptyItem';
+import { addItem } from '../services/addItem';
+import { initItemFields } from '../services/initItemFields';
 import { controller } from '../utils/controllerWrapper';
-
 export const handleAddItem = controller(async (req, res) => {
   const { inventoryId, createdBy } = req.body;
-  const result = await addItem(inventoryId, createdBy);
-  res.json(result);
+
+  const item = await addItem(inventoryId, createdBy);
+  const fields = await initItemFields(item.id, item.inventory_id);
+
+  res.json({ item, fields });
 });
