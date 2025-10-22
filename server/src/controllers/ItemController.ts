1334f95cedf99ad45339482877e16d8fd1fc4f4d
Dominik Sedusov <dom1k11@outlook.com>
2025-10-22 18:50:16 +0300

    adding items via request defining item fields values

--- server/src/controllers/ItemController.ts
+++ server/src/controllers/ItemController.ts
@@ -2,11 +2,23 @@ import { deleteItems } from '../queries/deleteItems';
 import { addItem } from '../services/addItem';
 import { initItemFields } from '../services/initItemFields';
 import { controller } from '../utils/controllerWrapper';
+import { insertFieldValues } from '../queries/insertFieldValues';
 export const handleAddItem = controller(async (req, res) => {
-  const { inventoryId, createdBy } = req.body;
+  const { inventoryId, createdBy, fields } = req.body;
   const item = await addItem(inventoryId, createdBy);
-  const fields = await initItemFields(item.id, item.inventory_id);
-  res.json({ item, fields });
+
+  if (fields && fields.length > 0) {
+    const values = fields.map((f) => ({
+      item_id: item.id,
+      field_template_id: f.field_template_id,
+      value: f.value,
+    }));
+    await insertFieldValues(values);
+    res.json({ item, fields: values });
+  }
+
+  const defaults = await initItemFields(item.id, item.inventory_id);
+  res.json({ item, fields: defaults });
 });
 
 export const handleDeleteItems = controller(async (req, res) => {
