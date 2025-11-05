ad2ae64262933b17309a2f7e98c4b942c2c32799
Dominik Sedusov <dom1k11@outlook.com>
2025-11-05 14:37:19 +0200

    auth validation on server

--- server/src/routes/authentication.ts
+++ server/src/routes/authentication.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { handleLogin } from '../controllers/loginController';
 import { handleRegister } from '../controllers/registerController';
+import { validate } from '../middlewares/validateMiddleware';
+import { loginSchema, registerSchema } from '../validation/authSchemas';
 const router = Router();
 
-router.post('/register', handleRegister);
-router.post('/login', handleLogin);
+router.post('/register', validate(registerSchema), handleRegister);
+router.post('/login', validate(loginSchema), handleLogin);
 export default router;
